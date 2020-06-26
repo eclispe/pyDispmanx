388e40cddfad5f24791c85687abace531d18e7d8
Tim Clark <eclipse@sucs.org>
2020-06-26 18:22:32 +0100

    increase number of FPS calculator trials

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import pydispmanx, time, pygame
 print(pydispmanx.getDisplaySize())
 testlayer = pydispmanx.dispmanxLayer(3);
 pygame_surface = pygame.image.frombuffer(testlayer, testlayer.size, 'RGBA')
-trials = 10
+trials = 100
 start=time.time()
 for n in  range(int(trials/2)):
     pygame.draw.circle(pygame_surface, (255,0,0), (int(testlayer.size[0]/2), int(testlayer.size[1]/2)), int(min(testlayer.size)/4), 0)
